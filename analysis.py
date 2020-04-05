f944bfd85a7740817f85759beccabc714c160d43
DanielKuang <dk6548@mit.edu>
2020-04-05 19:34:47 -0400

    reformatted all python files and abstracted functions in analysis.py

--- analysis.py
+++ analysis.py
@@ -3,5 +3,28 @@ import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-p="recording_tmp" # Audio File title
-mysp.mysppaus(p,dir_path)
+# Note filename must be in same directory as myspsolution.praat
+
+
+def get_speech_rate(filename):
+    mysp.mypssr(filename, dir_path)
+
+
+def get_summary(filename):
+    mysp.mysptotal(filename, dir_path)
+
+
+def get_articulation_rate(filename):
+    mysp.myspatc(filename, dir_path)
+
+
+def get_num_pauses(filename):
+    mysp.mysppaus(filename, dir_path)
+
+
+def get_num_syllables(filename):
+    mysp.myspsyl(filename, dir_path)
+
+
+def get_gender(filename):
+    mysp.myspgend(filename, dir_path)
