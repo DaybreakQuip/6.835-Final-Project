f0769b427ff4994b4e833ba36ca516000253dd48
DanielKuang <dk6548@mit.edu>
2020-04-05 19:53:59 -0400

    init main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from analysis import *
 from record import record_to_file
 import os
+import time
+
+COUNTER = 0
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    print("6UAssist is starting...")
+    main()
+    print("Thank you for using 6UAssist")
