653574c4d8ba367ebb0fe8db9211c6c6ba99981c
Ze Hang Zheng <zehang@mit.edu>
2020-04-05 16:35:21 -0700

    analysis

--- analysis.py
+++ analysis.py
@@ -1,7 +1,28 @@
 import myspsolution as mysp
 import os
+import speech_recognition as sr
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 p="recording_tmp" # Audio File title
 mysp.mysppaus(p,dir_path)
+
+AUDIO_FILE = ("record_tmp.wav")
+
+# use the audio file as the audio source
+
+r = sr.Recognizer()
+
+with sr.AudioFile(AUDIO_FILE) as source:
+    # reads the audio file. Here we use record instead of
+    # listen
+    audio = r.record(source)
+
+try:
+    print("The audio file contains: " + r.recognize_google(audio))
+
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0} ".format(e))
