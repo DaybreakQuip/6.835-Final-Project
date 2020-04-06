d228ac1dfbef455e0c365cb32942c3a993c16c49
DanielKuang <dk6548@mit.edu>
2020-04-06 00:22:14 -0400

    added run_GUI and clean_tmps

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from record import record_to_file
 import os
 import time
+import tkinter
 
 from analysis import *
 from speaker import *
@@ -8,12 +9,20 @@ from utils import *
 
 COUNTER = 0
 
+def run_GUI():
+    pass 
+
 def main():
-    load_speech("hi world")
+    load_speech("6UAssist is starting...")
+    unload_speech()
+    
+    run_GUI()
+    clean_tmps()
+
+    load_speech("Thank you for using 6UAssist")
     unload_speech()
 
 if __name__ == '__main__':
     print("6UAssist is starting...")
     main()
-    clean_tmps()
     print("Thank you for using 6UAssist")
