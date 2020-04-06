8fcc3b06237f4c4390ee206c32c6552945151b0d
DanielKuang <dk6548@mit.edu>
2020-04-06 00:36:12 -0400

    added email functionality

--- main.py
+++ main.py
@@ -6,21 +6,25 @@ import tkinter
 from analysis import *
 from speaker import *
 from utils import *
+import os
 
 COUNTER = 0
 
 def run_GUI():
     pass 
 
+
 def main():
     load_speech("6UAssist is starting...")
     unload_speech()
-    
+
     run_GUI()
     clean_tmps()
 
     load_speech("Thank you for using 6UAssist")
     unload_speech()
+
+    # os.remove(DATA_FILENAME) # remove data file of user
 
 if __name__ == '__main__':
     print("6UAssist is starting...")
