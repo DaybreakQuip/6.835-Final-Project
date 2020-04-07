9d78a8667a61aee431e0d63deb1f6853a3cb453a
Ze Hang Zheng <zehang@mit.edu>
2020-04-06 21:17:39 -0700

    recording and deletion

--- MultiThreading.py
+++ MultiThreading.py
@@ -1,38 +1,41 @@
 import threading
 from tkinter import *
 import time
+from record import *
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 runner1 = None
-runner2 = None
-
+counter = 0
+last_file_counter = 0
 def record_thread():
+    global counter
     runner2 = threading.currentThread()
     while getattr(runner2, "do_run", True):
-        print("Hi, I am your first thread talking.")
-        time.sleep(3)
-    print("Stopping first thread")
+        record_to_file("recording_" + str(counter))
+        counter += 1
+    runner2 = threading.Thread(target=stop_thread,args=())
+    runner2.start()
 
 def stop_thread():
+    global counter
+    global last_file_counter
+    temp = counter
     runner1 = threading.currentThread()
-    while getattr(runner1, "do_run", True):
-        print("Hi, I am your second thread talking.")
-        time.sleep(3)
-    print("Stopping second thread")
+    for i in range(last_file_counter, counter):
+        os.remove(dir_path + "\\recording_" + str(i) + ".wav")
+    last_file_counter = temp
+    print("stopped deleting")
 
 def start_command():
     global runner1
-    global runner2
     runner1 = threading.Thread(target=record_thread,args=())
     runner1.start()
-    runner2 = threading.Thread(target=stop_thread,args=())
-    runner2.start()
     switch.configure(text="Stop", command=stop_command)
 
 def stop_command():
     global runner1
-    global runner2
     runner1.do_run = False
-    runner2.do_run = False
     switch.configure(text="Start", command=start_command)
 
 root = Tk()
